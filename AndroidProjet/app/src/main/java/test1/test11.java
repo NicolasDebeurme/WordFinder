52fb2cd732b3025e34e9978f0715e9546dd119c7
Hakim <71723927+HakimDuparcq@users.noreply.github.com>
2021-11-05 15:06:56 +0100

    recycler_copi_tuto

--- AndroidProjet/app/src/main/java/test1/test11.java
+++ AndroidProjet/app/src/main/java/test1/test11.java
@@ -3,17 +3,30 @@ package test1;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.androidprojet.R;
 
 public class test11 extends AppCompatActivity {
 
+    RecyclerView recyclerView;
+
     String s1[], s2[];
-    int images[] = {}
+    int images[] = {R.drawable.img,R.drawable.img,R.drawable.img,R.drawable.img,R.drawable.img,R.drawable.img,R.drawable.img,R.drawable.img};
 
     @Override
     protected void onCreate(Bundle saveInstanceState){
         super.onCreate(saveInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById((R.id.recyclerView));
+
+        s1= getResources().getStringArray(R.array.programming_language);
+        s2= getResources().getStringArray(R.array.description);
+
+        MyAdapter myAdapter = new MyAdapter(this, s1,s2,images);
+        recyclerView.setAdapter(myAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }

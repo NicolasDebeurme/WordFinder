6103ab4511f6be2e0157804100012c359d0dae03
Hakim <71723927+HakimDuparcq@users.noreply.github.com>
2021-11-14 14:36:34 +0100

    search bar

---
+++ AndroidProjet/app/src/main/java/test_search_bar/Main_search_bar.java
@@ -0,0 +1,62 @@
+package test_search_bar;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
+
+import com.example.androidprojet.R;
+
+public class Main_search_bar extends AppCompatActivity {
+
+    ListView listView;
+    String[] name= {"Chris","Chris2","Chris3","Chris","Chris5","Chris","Chris10"};
+
+    ArrayAdapter<String> arrayAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_search_bar);
+
+        listView= findViewById(R.id.listview);
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,name);
+
+        listView.setAdapter(arrayAdapter);
+    }
+
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+
+        getMenuInflater().inflate(R.menu.menu,menu);
+
+        MenuItem menuItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Type here to search");
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.d("succes", query);
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                arrayAdapter.getFilter().filter(newText);
+                return false;
+            }
+        });
+
+        return super.onCreateOptionsMenu(menu);
+
+    }
+}
